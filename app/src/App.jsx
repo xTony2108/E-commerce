c09610e46a7a522028794f325fc1e6c433ba3a94
Antonino Alampi <Alampiantonino99@gmail.com>
2024-01-09 00:41:02 +0100

    add: created routes with react router dom

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Cart } from "./pages/Cart";
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 };
