e5bf7efd7eecb59f4401a1f2ae4ec001ca6667ab
xTony2108 <alampiantonino99@gmail.com>
2024-01-23 17:28:25 +0100

    fix: fixed navbar color and width

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,19 +4,16 @@ export const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="border-b-2 border-black">
-          <div className="flex justify-between items-center max-w-screen-2xl m-auto py-2">
-            <div>
-              <p className="text-3xl">Logo</p>
+          <div className="flex justify-between items-center max-w-screen-lg m-auto py-10" role="presentation" aria-hidden="true">
+            <div role="presentation" aria-hidden="true">
+              <p className="text-2xl text-white" role="logo">GamerGear Hub</p>
             </div>
-            <div className="flex gap-8">
+            <div className="flex gap-8 text-white" role="presentation" aria-hidden="true">
               <Link to="/">Home</Link>
-              <Link to="/products">Products</Link>
-              <Link to="/cart">Cart</Link>
+              <Link to="/products">Prodotti</Link>
               <Link to="/login">Login</Link>
             </div>
           </div>
-        </div>
       </nav>
     </>
   );
