bd42419c6b87b48d054e55321450b68b74cfe154
xTony2108 <alampiantonino99@gmail.com>
2024-01-23 19:08:45 +0100

    add: created description section

---
+++ app/src/components/Description.jsx
@@ -0,0 +1,34 @@
+export const Description = () => {
+  return (
+    <>
+      <div className="flex gap-4 border-b border-white pb-8">
+        <div className="flex flex-col gap-2 flex-1">
+          <h2 className="text-[#AEDFF7] font-bold text-xl">
+            Le Migliori Scelte!
+          </h2>
+          <p className="text-white">
+            Scopri il meglio dell'hardware PC con la nostra selezione premium,
+            garantendo prestazioni eccezionali.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 flex-1">
+          <h2 className="text-[#AEDFF7] font-bold text-xl">
+            Prestazioni Eccellenti
+          </h2>
+          <p className="text-white">
+            Siamo qui per offrirti il massimo in termini di prestazioni. Trova
+            l'hardware PC perfetto per le tue esigenze tra le nostre opzioni di
+            alta qualità.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 flex-1">
+          <h2 className="text-[#AEDFF7] font-bold text-xl">Ultimi Arrivi!</h2>
+          <p className="text-white">
+            Resta aggiornato con le ultime novità dell'hardware PC. Scopri le
+            soluzioni più innovative per potenziare il tuo sistema.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
