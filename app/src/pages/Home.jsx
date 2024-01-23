a52af88eb30a48502808c5845bd0ed9c00572b21
xTony2108 <alampiantonino99@gmail.com>
2024-01-23 19:08:45 +0100

    add: created description section

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import { HeroSection } from "../components/HeroSection";
+import { Description } from "../components/Description";
 
 export const Home = () => {
   return (
     <>
       <HeroSection />
+      <div className="bg-[#121420] py-32">
+        <div className="max-w-screen-lg m-auto">
+          <Description />
+        </div>
+      </div>
     </>
   );
 };
