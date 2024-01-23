fed4055a3573cc39b0ce4297605f0b6fad3db772
xTony2108 <alampiantonino99@gmail.com>
2024-01-23 19:04:00 +0100

    add:created hero section

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,16 +4,26 @@ export const Navbar = () => {
   return (
     <>
       <nav>
-          <div className="flex justify-between items-center max-w-screen-lg m-auto py-10" role="presentation" aria-hidden="true">
-            <div role="presentation" aria-hidden="true">
-              <p className="text-2xl text-white" role="logo">GamerGear Hub</p>
-            </div>
-            <div className="flex gap-8 text-white" role="presentation" aria-hidden="true">
-              <Link to="/">Home</Link>
-              <Link to="/products">Prodotti</Link>
-              <Link to="/login">Login</Link>
-            </div>
+        <div
+          className="flex justify-between items-center max-w-screen-lg m-auto py-10 relative z-50"
+          role="presentation"
+          aria-hidden="true"
+        >
+          <div role="presentation" aria-hidden="true">
+            <p className="text-2xl text-white" role="logo">
+              GamerGear Hub
+            </p>
           </div>
+          <div
+            className="flex gap-8 text-white"
+            role="presentation"
+            aria-hidden="true"
+          >
+            <Link to="/">Home</Link>
+            <Link to="/products">Prodotti</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
       </nav>
     </>
   );
