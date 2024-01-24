7900ff8db4d39c94078ec82cb6d572e1d26b0a8a
Antonino Alampi <Alampiantonino99@gmail.com>
2024-01-24 23:22:16 +0100

    fix: changed heading colors, buttons color  and faq section bg color

--- app/src/components/BottomCta.jsx
+++ app/src/components/BottomCta.jsx
@@ -5,7 +5,7 @@ export const BottomCta = () => {
     <section>
       <div className="bg-[#191B1F] py-16 bg-bottomCta bg-blend-overlay bg-cover bg-center">
         <div className="flex flex-col items-center gap-5 max-w-screen-2xl m-auto">
-          <h2 className="text-[#AEDFF7] font-bold text-3xl max-2xl:text-xl">
+          <h2 className="text-[#F0C039] font-bold text-3xl max-2xl:text-xl">
             Massimizza il tuo setup!
           </h2>
           <p className="text-white text-lg">
--- app/src/components/FeedbackCard.jsx
+++ app/src/components/FeedbackCard.jsx
@@ -1,6 +1,6 @@
 export const FeedbackCard = ({ comment, name }) => {
   return (
-    <div className="rounded-xl border-2 border-white p-4 flex flex-col gap-4 flex-1 text-lg">
+    <div className="rounded-xl border-2 border-[#F0C039] p-4 flex flex-col gap-4 flex-1 text-lg">
       <p className="text-white">{comment}</p>
       <div className="w-24 h-24 bg-white rounded-full"></div>
       <p className="text-white">{name}</p>
