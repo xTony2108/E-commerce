43ca8b32e93eed0d4db090288448cddc179f61d4
Antonino Alampi <Alampiantonino99@gmail.com>
2024-02-01 13:47:52 +0100

    fix: changed button text color to black

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -4,7 +4,7 @@ export const Button = ({ text, filled, route }) => {
   return filled ? (
     <Link
       to={route}
-      className="min-w-32 text-center text-lg text-white bg-[#00C6C1] px-5 rounded-lg"
+      className="min-w-32 text-center text-lg text-black bg-[#00C6C1] px-5 rounded-lg"
     >
       {text}
     </Link>
